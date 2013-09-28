45619e1e0f1f6e2df0b7da389b501f80b7cc5bc7
Petrov Sergey <petrov@trilan.ru>
2013-09-28 19:32:17 +0400

    fix core urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,7 @@ from .views import FriendListView
 
 urlpatterns = patterns(
     '',
-    url(r'^friends/$', FriendListView.as_view(), name='friendlist_view'),
+    url(r'^start/$', FriendListView.as_view(), name='friendlist_view'),
     url(r'^record/$', 'core.views.update_profile',
         name='coords_update_view'),
     url(r'^venues/(?P<pk>\d+)/$', 'core.views.get_venues',
