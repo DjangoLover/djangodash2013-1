58efa17c4ac6888ab7712bd3daa1dbb0d9cf82f9
Petrov Sergey <petrov@trilan.ru>
2013-09-28 10:33:23 +0400

    launched a project

---
+++ meetpoint/settings/production.py
@@ -0,0 +1,21 @@
+from .common import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project',
+    }
+}
+
+PUBLIC_ROOT = os.path.join(os.sep, 'var', 'www', 'project', 'public')
+STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
+MEDIA_ROOT = os.path.join(PUBLIC_ROOT, 'media')
+
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', TEMPLATE_LOADERS),
+)
+
+# ALLOWED_HOSTS = ['192.241.165.151']
+
+PREPEND_WWW = True
