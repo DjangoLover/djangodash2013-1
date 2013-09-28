d70aaa6002fc815c7d1edd198d7bcd2ba624fb55
Vadim Selyakov <selyakovadim@gmail.com>
2013-09-28 18:16:31 +0400

    Fixed errors in user model

--- apps/profile/models.py
+++ apps/profile/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class Profile(AbstractUser):
 
-    last_update = models.DateField(auto_now=True)
-    latitude = models.DecimalField(max_digits=5, decimal_places=2)
-    longitude = models.DecimalField(max_digits=5, decimal_places=2)
+    last_update = models.DateField(
+        auto_now=True
+    )
+    latitude = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+    longitude = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
