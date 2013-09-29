c6fc06b640358522c1151e9fd9e41d7961f3cfa2
Petrov Sergey <petrov@trilan.ru>
2013-09-30 02:27:24 +0400

    FFFINAL COMMIT BLGDAD

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -9,8 +9,6 @@ urlpatterns = patterns(
         name='coords_update_view'),
     url(r'^venues/(?P<pk>\d+)/$', VenueView.as_view(),
         name='venues_view'),
-    url(r'^how/$', TemplateView.as_view(template_name="how.html"),
-        name='how_view'),
     url(r'^home/$', TemplateView.as_view(template_name="home.html")),
     url(r'^$', RedirectView.as_view(url="home/")),
 )
