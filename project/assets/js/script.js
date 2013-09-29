7d50d1d9f9661d33557a579cb8e11d3d27730daf
Petrov Sergey <petrov@trilan.ru>
2013-09-29 21:27:20 +0400

    exp 2

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -1,9 +1,10 @@
 //= require plugins/bootstrap.min
 //= require plugins/html5shiv
 //= require plugins/less-1.3.3.min
-//= require plugins/gmapi
-//= require plugins/gmaps
 
+//= require plugins/moment.min
+//= require plugins/moment-timezone.min
+//= require plugins/moment-timezone-data
 //= require_self
 
 $(document).ready(function(){
@@ -16,65 +17,65 @@ $(document).ready(function(){
     $('.last_update').html($last_update_formatted.toString());
   }
 
-  map = new GMaps({
-    div: '#map',
-    lat: -12.043333,
-    lng: -77.028333,
-    height: '400px',
-  });
+  // map = new GMaps({
+  //   div: '#map',
+  //   lat: -12.043333,
+  //   lng: -77.028333,
+  //   height: '400px',
+  // });
 
-  map.addMarker({
-    position: map.getCenter(),
-    draggable: true,
-  });
+  // map.addMarker({
+  //   position: map.getCenter(),
+  //   draggable: true,
+  // });
 
-  GMaps.geolocate({
-    success: function(position) {
-      console.log('success');
-      map.setCenter(position.coords.latitude, position.coords.longitude);
-      map.removeMarkers();
-      map.addMarker({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        draggable: true,
-        clickable: true
-      });
-      $("#status").load('/record/', {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      });
-    },
-    error: function(error) {
-      alert('Geolocation failed: '+error.message);
-    },
-    not_supported: function() {
-      alert("Your browser does not support geolocation");
-    },
-  });
+  // GMaps.geolocate({
+  //   success: function(position) {
+  //     console.log('success');
+  //     map.setCenter(position.coords.latitude, position.coords.longitude);
+  //     map.removeMarkers();
+  //     map.addMarker({
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude,
+  //       draggable: true,
+  //       clickable: true
+  //     });
+  //     $("#status").load('/record/', {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude,
+  //     });
+  //   },
+  //   error: function(error) {
+  //     alert('Geolocation failed: '+error.message);
+  //   },
+  //   not_supported: function() {
+  //     alert("Your browser does not support geolocation");
+  //   },
+  // });
 
-  $('.friend').click(function(){
-    marker_position = map.markers[0].getPosition(noWrap=true);
-    $("#status").load('/record/', {
-      lat: marker_position.lat(),
-      lng: marker_position.lng(),
-    });
-    $.ajax({
-      type: "POST",
-      data: {lat: marker_position.lat(), lng: marker_position.lng()},
-      url: $(this).attr('href'),
-      success: function(data){
-        $('.block_main').html(data);
-        $friend = $('.block_main .friend_coords');
-        local_time();
-        map.addMarker({
-          lat: $friend.data("lat"),
-          lng: $friend.data("lng"),
-          draggable: true,
-          clickable: true
-        });
-      }
-    });
-    return false;
-  });
+  // $('.friend').click(function(){
+  //   marker_position = map.markers[0].getPosition(noWrap=true);
+  //   $("#status").load('/record/', {
+  //     lat: marker_position.lat(),
+  //     lng: marker_position.lng(),
+  //   });
+  //   $.ajax({
+  //     type: "POST",
+  //     data: {lat: marker_position.lat(), lng: marker_position.lng()},
+  //     url: $(this).attr('href'),
+  //     success: function(data){
+  //       $('.block_main').html(data);
+  //       $friend = $('.block_main .friend_coords');
+  //       local_time();
+  //       map.addMarker({
+  //         lat: $friend.data("lat"),
+  //         lng: $friend.data("lng"),
+  //         draggable: true,
+  //         clickable: true
+  //       });
+  //     }
+  //   });
+  //   return false;
+  // });
 
 });
